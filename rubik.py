5178e20e0cee835f2cf51b10c3b3660309a21227
alotofdavid <david.adams94@gmail.com>
2014-03-29 22:20:03 -0700

    can create scramble instances and check if an algorithm is a solution to scramble

--- rubik.py
+++ rubik.py
@@ -2,24 +2,48 @@ import re
 SOLUTION_REGEX = re.compile("^[RLUDFBMES' 2xyz]*$")
 MOVE_REGEX = re.compile("^[RLUDFMBESxyz]['2]?$")
 
-move_dict = {"U":0,"L":1,"F":2,"R":3,"B":4,"D":5}
+FACE_MOVES = ["U","D","F","B","L","R"]
+SLICE_MOVES = ["M","E","S"]
+ROTATIONS = ["x","y","z"]
+move_dict = {"U":0,"L":1,"F":2,"R":3,"B":4,"D":5, "E":0, "M":1, "S": 2, "x":0, "y":1, "z":2}
+
 
 class Algorithm(object):
-	def __init__(self,move_string):
+	def __init__(self,alg):
+		moves = alg.split()
+		assert(valid_moves(moves))
+		self.move_count = 0
 		self.moves = []
-		for move in move_string:
-			self.moves.append(Move(move))
+		for move in moves:
+			m = Move(move)
+			if m.letter in FACE_MOVES:
+				self.move_count += 1
+			elif m.letter in SLICE_MOVES:
+				self.move_count += 2
+			self.moves.append(m)
+
 
 	def __repr__(self):
 		return " ".join([str(m) for m in self.moves])
 
+class Scramble(Algorithm):
+	def __init__(self,move_string_arr):
+		Algorithm.__init__(self,move_string_arr)
+
+	def solution(self, sol): 
+		c = Cube()
+		c.apply_alg(self)
+		c.apply_alg(sol)
+		return c.solved()
+
+
 class Move(object):
 	def __init__(self,move):
 		if (len(move)==1):
-			self.face = move
+			self.letter = move
 			self.num = 1
 		if (len(move)==2):
-			self.face = move[:1]
+			self.letter = move[:1]
 			rest = move[1:]
 			if rest == "'":
 				self.num = 3
@@ -27,7 +51,7 @@ class Move(object):
 				self.num = 2
 
 	def __repr__(self):
-		return self.face + str(self.num)
+		return self.letter + str(self.num)
 
 
 
@@ -38,39 +62,11 @@ class Cube(object):
 	def __repr__(self):
 		return str(self.cube)
 
-	def do_move(self, move):
-		self.twist(move_dict[move.face], move.num)
-
-	def twist(self, face, amount):
-		for _ in range(amount):
-			self.rotate(face)
-
-	def rotate(self, face):
-		#U
-		if face==0:
-			#top row of l,f,r,b cycle
-			t = self.cube[1][0]
-			self.cube[1][0] = self.cube[2][0]
-			self.cube[2][0] = self.cube[3][0]
-			self.cube[3][0] = self.cube[4][0]
-			self.cube[4][0] = t
-			#rotate U face
-
-		#L
-		elif face==1:
-			return
-		#F
-		elif face==2:				
-			return
-		#R
-		elif face==3:
-			return
-		#B
-		elif face==4:
-			return
-		#D
-		elif face==5:
-			return
+	def solved(self):
+		for face in range(6):
+			if len(set(self.cube[face][0]).union(set(self.cube[face][1])).union(set(self.cube[face][2]))) > 1:
+				return False
+		return True
 
 	def cycle_stickers(self, *args):
 		t = self.cube[args[len(args)-1][0]][args[len(args)-1][1]][args[len(args)-1][2]]
@@ -85,10 +81,91 @@ class Cube(object):
 		t = self.cube[args[len(args)-1][0]][args[len(args)-1][1]]		
 		loop = range(len(args))
 		loop.reverse()
-		for i in range(len(args)):
+		for i in loop:
 			if i > 0:
 				self.cube[args[i][0]][args[i][1]] = self.cube[args[i-1][0]][args[i-1][1]]
 		self.cube[args[0][0]][args[0][1]] = t
+
+	def rotate_face(self, face):
+		#rotate the stickers on the face
+		self.cycle_stickers([face,0,0],[face,0,2],[face,2,2],[face,2,0])
+		self.cycle_stickers([face,0,1],[face,1,2],[face,2,1],[face,1,0])
+
+		#U
+		if face==0:
+			self.cycle_rows([4,0],[3,0],[2,0],[1,0])
+		#L
+		elif face==1:
+			self.cycle_stickers([0,0,0],[2,0,0],[5,0,0],[4,2,2])
+			self.cycle_stickers([0,1,0],[2,1,0],[5,1,0],[4,1,2])
+			self.cycle_stickers([0,2,0],[2,2,0],[5,2,0],[4,0,2])
+		#F
+		elif face==2:				
+			self.cycle_stickers([0,2,0],[3,0,0],[5,0,2],[1,2,2])
+			self.cycle_stickers([0,2,1],[3,1,0],[5,0,1],[1,1,2])
+			self.cycle_stickers([0,2,2],[3,2,0],[5,0,0],[1,0,2])
+		#R
+		elif face==3:
+			self.cycle_stickers([0,2,2],[4,0,0],[5,2,2],[2,2,2])
+			self.cycle_stickers([0,1,2],[4,1,0],[5,1,2],[2,1,2])
+			self.cycle_stickers([0,0,2],[4,2,0],[5,0,2],[2,0,2])
+		#B
+		elif face==4:
+			self.cycle_stickers([0,0,0],[1,2,0],[5,2,2],[3,0,2])
+			self.cycle_stickers([0,0,1],[1,1,0],[5,2,1],[3,1,2])
+			self.cycle_stickers([0,0,2],[1,0,0],[5,2,0],[3,2,2])
+		#D
+		elif face==5:
+			self.cycle_rows([1,2],[2,2],[3,2],[4,2])
+
+	def slice(self, axis):
+		#E
+		if axis==0:
+			self.cycle_rows([1,1],[2,1],[3,1],[4,1])
+
+		#M
+		elif axis==1:
+			self.cycle_stickers([0,0,1],[2,0,1],[5,0,1],[4,2,1])
+			self.cycle_stickers([0,1,1],[2,1,1],[5,1,1],[4,1,1])
+			self.cycle_stickers([0,2,1],[2,2,1],[5,2,1],[4,0,1])
+
+		#S	
+		elif axis==2:
+			self.cycle_stickers([0,1,0],[1,2,1],[5,1,2],[3,0,1])
+			self.cycle_stickers([0,1,1],[1,1,1],[5,1,1],[3,1,1])
+			self.cycle_stickers([0,1,2],[1,0,1],[5,1,0],[3,2,1])
+
+	def rotate(self, axis):
+		#x
+		if axis == 0: 
+			self.apply_move(Move("R"))
+			self.apply_move(Move("L'"))
+			self.apply_move(Move("M'"))
+		#y
+		elif axis == 1:
+			self.apply_move(Move("U"))
+			self.apply_move(Move("E'"))
+			self.apply_move(Move("D'"))
+		#z
+		elif axis == 2:
+			self.apply_move(Move("B'"))
+			self.apply_move(Move("F"))
+			self.apply_move(Move("S'"))
+
+	def apply_alg(self, alg):
+		for move in alg.moves:
+			self.apply_move(move)
+
+	def apply_move(self, move):
+		if move.letter in FACE_MOVES:
+			for _ in range(move.num):
+				self.rotate_face(move_dict[move.letter])
+		elif move.letter in SLICE_MOVES:
+			for _ in range(move.num):
+				self.slice(move_dict[move.letter])
+		elif move.letter in ROTATIONS:
+			for _ in range(move.num):
+				self.rotate(move_dict[move.letter])
 
 def parse_alg_string(alg):
 	moves = alg.split()
@@ -103,3 +180,8 @@ def valid_moves(moves):
 			print("{move} does not match regex".format(move=move))
 			return False
 	return True
+
+if __name__ == "__main__":
+	c = Cube()
+
+
