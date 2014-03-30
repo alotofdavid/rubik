bfe7d300648d41b02c60760cd93f0f5cfd9b5efb
alotofdavid <david.adams94@gmail.com>
2014-03-29 20:19:15 -0700

    Cube can cycle stickers and rows of stickers. Savepoint for if I fuck up while implementing the description for all the legal moves

--- rubik.py
+++ rubik.py
@@ -2,6 +2,8 @@ import re
 SOLUTION_REGEX = re.compile("^[RLUDFBMES' 2xyz]*$")
 MOVE_REGEX = re.compile("^[RLUDFMBESxyz]['2]?$")
 
+move_dict = {"U":0,"L":1,"F":2,"R":3,"B":4,"D":5}
+
 class Algorithm(object):
 	def __init__(self,move_string):
 		self.moves = []
@@ -36,11 +38,59 @@ class Cube(object):
 	def __repr__(self):
 		return str(self.cube)
 
-	def twist(self, move):
-		return None
+	def do_move(self, move):
+		self.twist(move_dict[move.face], move.num)
 
+	def twist(self, face, amount):
+		for _ in range(amount):
+			self.rotate(face)
 
-def parse_alg(alg):
+	def rotate(self, face):
+		#U
+		if face==0:
+			#top row of l,f,r,b cycle
+			t = self.cube[1][0]
+			self.cube[1][0] = self.cube[2][0]
+			self.cube[2][0] = self.cube[3][0]
+			self.cube[3][0] = self.cube[4][0]
+			self.cube[4][0] = t
+			#rotate U face
+
+		#L
+		elif face==1:
+			return
+		#F
+		elif face==2:				
+			return
+		#R
+		elif face==3:
+			return
+		#B
+		elif face==4:
+			return
+		#D
+		elif face==5:
+			return
+
+	def cycle_stickers(self, *args):
+		t = self.cube[args[len(args)-1][0]][args[len(args)-1][1]][args[len(args)-1][2]]
+		loop = range(len(args))
+		loop.reverse()
+		for i in loop:
+			if i > 0:
+				self.cube[args[i][0]][args[i][1]][args[i][2]] = self.cube[args[i-1][0]][args[i-1][1]][args[i-1][2]]
+		self.cube[args[0][0]][args[0][1]][args[0][2]] = t
+
+	def cycle_rows(self, *args):
+		t = self.cube[args[len(args)-1][0]][args[len(args)-1][1]]		
+		loop = range(len(args))
+		loop.reverse()
+		for i in range(len(args)):
+			if i > 0:
+				self.cube[args[i][0]][args[i][1]] = self.cube[args[i-1][0]][args[i-1][1]]
+		self.cube[args[0][0]][args[0][1]] = t
+
+def parse_alg_string(alg):
 	moves = alg.split()
 	if valid_moves(moves):
 		return Algorithm(moves)
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -7,3 +7,5 @@ def test_valid_moves():
 def test_cube():
 	c = Cube()
 	assert(c.cube==[[[i for _ in xrange(3)] for _ in xrange(3)] for i in xrange(6)])
+	c.cycle_stickers([1,2,2],[2,2,2],[5,0,0])
+	assert(c.cube[1][2][2] == 5)
